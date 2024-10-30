176bdc130810106c6c1f95e3837456d5b2550225
wahidcodes <105361947+wahidcodes@users.noreply.github.com>
2024-10-31 01:39:41 +0530

    modified entry path in webpack

--- job-app/webpack/webpack.common.js
+++ job-app/webpack/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './static/api.js',
+    entry: './src/index.js',
     output: {
       filename: 'index.js',
       path: path.resolve(__dirname, 'dist'),
